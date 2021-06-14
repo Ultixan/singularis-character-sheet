97ac28d83a3559e2f03f06f611dab1cfc9c5c059
ultixan <ultixan@gmail.com>
2021-06-14 23:29:30 +0100

    Treat the skill row as a div, and make the css key directly off of it.

--- components.js
+++ components.js
@@ -1,17 +1,15 @@
 class SkillRow extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <div class="row">
-      <label>
-        <input type="radio" name="focus"/>
-        <svg viewbox="0 0 8 8">
-          <circle cx="4" cy="4" r="3" stroke="black" stroke-width="1" fill="black"/>
-          <circle class="unchecked" cx="4" cy="4" r="3" stroke="black" stroke-width="1" fill="white"/>
-        </svg>
-      </label>
-      <input type="text" class="name"/>
-      <input type="text" class="value"/>
-    </div>
+    <label>
+      <input type="radio" name="focus"/>
+      <svg viewbox="0 0 8 8">
+        <circle cx="4" cy="4" r="3" stroke="black" stroke-width="1" fill="black"/>
+        <circle class="unchecked" cx="4" cy="4" r="3" stroke="black" stroke-width="1" fill="white"/>
+      </svg>
+    </label>
+    <input type="text" class="name"/>
+    <input type="text" class="value"/>
     `
   }
 }
